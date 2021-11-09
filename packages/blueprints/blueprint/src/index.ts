98b207720a8fd627ced12e91de50b7b1b3209958
Alex Forsyth <alex-git@amazon.com>
2021-11-09 12:21:32 -0500

    overwriting

--- packages/blueprints/blueprint/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './blueprint';
-export * from './context';
-
-import defaults_ from './defaults.json';
-export const defaults = defaults_;
--- packages/utils/blueprint-utils/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './build-blueprint';
-export * from './build-defaults';
-export * from './build-index';
-export * from './build-package-json';
-export * from './build-readme';
-
-export * from './introspect-blueprint';
