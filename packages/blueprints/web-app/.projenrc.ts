4eb2b74bca7295bf5bdd52a102a5536ed161445d
Alex Forsyth <alex-git@amazon.com>
2021-11-09 17:32:53 -0500

    feat(web-app): adding web-app blueprint

--- packages/blueprints/web-app/.projenrc.ts
+++ packages/blueprints/web-app/.projenrc.ts
@@ -3,25 +3,13 @@ import {ProjenBlueprint} from '@caws-blueprint-util/blueprint-projen'
 const blueprint = new ProjenBlueprint({
   defaultReleaseBranch: 'main',
   name: 'web-app',
-  license: 'MIT',
-  projenrcTs: true,
-  sampleCode: false,
-  github: false,
-  eslint: false,
-  jest: false,
-  npmignoreEnabled: true,
-  projenDuringBuild: false,
-  tsconfig: {
-    compilerOptions: {
-      esModuleInterop: true,
-      noImplicitAny: false,
-    },
-  },
   authorName: 'caws-blueprints',
   copyrightOwner: 'Amazon.com',
+  projenrcTs: true,
 
   /* Runtime dependencies of this blueprint. */
   deps: [
+    'projen',
     '@caws-blueprint/caws.blueprint',
     '@caws-blueprint-component/caws-source-repositories',
     '@caws-blueprint-util/blueprint-utils',
@@ -36,8 +24,8 @@ const blueprint = new ProjenBlueprint({
   publishingOrganization: 'caws',
   /* Build dependencies for this module. */
   devDeps: [
-    'ts-node', 
-    '@caws-blueprint-tool/blueprint-cli',
+    'ts-node',
+    '@caws-blueprint-util/blueprint-cli',
     '@caws-blueprint-util/blueprint-projen'
   ],
   /* Add release management to this project. */
