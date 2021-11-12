edc6db06eddb96a1732b674ece3902a58ebaf926
Mike Landi <miclanm@amazon.com>
2021-11-12 15:18:32 -0500

    Support development workspaces through cblueprint component

--- packages/blueprints/web-app/.projenrc.ts
+++ packages/blueprints/web-app/.projenrc.ts
@@ -1,4 +1,4 @@
-import {ProjenBlueprint} from '@caws-blueprint-util/blueprint-projen'
+import {ProjenBlueprint} from '@caws-blueprint-util/blueprint-projen';
 
 const blueprint = new ProjenBlueprint({
   defaultReleaseBranch: 'main',
@@ -14,11 +14,11 @@ const blueprint = new ProjenBlueprint({
     '@caws-blueprint-component/caws-source-repositories',
     '@caws-blueprint-util/blueprint-utils',
     '@caws-blueprint-component/caws-workflows',
-    '@caws-blueprint-component/caws-environments'
+    '@caws-blueprint-component/caws-environments',
+    '@caws-blueprint-component/caws-workspaces',
   ],
   /* The description is a short string that helps people understand the purpose of the blueprint. */
-  description:
-    'This blueprint creates and deploys a web application',
+  description: 'This blueprint creates and deploys a web application',
   /* The "name" in package.json. In form @caws-blueprints/:organization.:name */
   packageName: '@caws-blueprint/caws.web-app',
   publishingOrganization: 'caws',
@@ -26,13 +26,15 @@ const blueprint = new ProjenBlueprint({
   devDeps: [
     'ts-node',
     '@caws-blueprint-util/blueprint-cli',
-    '@caws-blueprint-util/blueprint-projen'
+    '@caws-blueprint-util/blueprint-projen',
   ],
   /* Add release management to this project. */
   // release: undefined,
   keywords: ['blueprint', 'webapp', 'java', 'react'],
   homepage: 'https://aws.amazon.com/',
   /* Add media url links to this project */
-  mediaUrls: ['https://d1.awsstatic.com/logos/aws-logo-lockups/poweredbyaws/PB_AWS_logo_RGB_stacked_REV_SQ.91cd4af40773cbfbd15577a3c2b8a346fe3e8fa2.png'],
+  mediaUrls: [
+    'https://d1.awsstatic.com/logos/aws-logo-lockups/poweredbyaws/PB_AWS_logo_RGB_stacked_REV_SQ.91cd4af40773cbfbd15577a3c2b8a346fe3e8fa2.png',
+  ],
 });
 blueprint.synth();
