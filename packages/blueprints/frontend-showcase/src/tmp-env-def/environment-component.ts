a1e967e92d0f81362b861535143383703ac67ff6
Alex Forsyth <alex-git@amazon.com>
2022-03-23 15:29:39 -0400

    fix(frontend-showcase): update to generate with entropy

--- packages/blueprints/frontend-showcase/src/tmp-env-def/environment-component.ts
+++ packages/blueprints/frontend-showcase/src/tmp-env-def/environment-component.ts
@@ -1,19 +1,69 @@
 import { Blueprint } from '@caws-blueprint/blueprints.blueprint';
 import { Component, YamlFile } from 'projen';
-import { EnvironmentDefinition } from './environment-definition';
+import { AccountConnection, EnvironmentDefinition } from './environment-definition';
+
+const stripSpaces = (str: string) => str.replace(/\s/g, '');
+
+/**
+ * Generates an extropy string of a max 5 length
+ * @param length number: max 10
+ * @returns string
+ */
+const getEntropy = (length?: number) => (Math.random() + 1).toString(36).slice(2, 2 + (length || 5));
 
 export class Environment extends Component {
   constructor(blueprint: Blueprint, environment: EnvironmentDefinition<any>) {
     super(blueprint);
+
+    const writtenEnvironment = {
+      name: environment.name,
+      description: environment.description || environment.name,
+      environmentType: environment.environmentType,
+    };
+
+    const connectedAccounts: {
+      environmentName: string;
+      name: string;
+    }[] = [];
+
+    /**
+     * keys of the environment definition that dont represent an account connection
+     */
+    const nonAccountKeys = new Set(Object.keys(writtenEnvironment));
+
+    // find all the account connections on the environment
+    Object.keys(environment)
+      .filter(key => !nonAccountKeys.has(key))
+      .forEach(accountkey => {
+        const account: AccountConnection<any> = environment[accountkey];
+        connectedAccounts.push({
+          environmentName: environment.name,
+          name: account.accountName,
+        });
+      });
+
+    // create the environment file
     new YamlFile(
       blueprint,
-      `environments/${environment.environmentName}.yaml`,
+      `environments/${stripSpaces(writtenEnvironment.name)}.yaml`,
       {
-        obj: {
-          ...environment,
-          type: 'environment',
-        },
+        readonly: false,
+        marker: false,
+        obj: writtenEnvironment,
       },
     );
+
+    // create all the linked accounts from the environment
+    connectedAccounts.forEach(account => {
+      new YamlFile(
+        blueprint,
+        `aws-account-to-environment/${stripSpaces(account.name)}-${getEntropy(5)}.yaml`,
+        {
+          readonly: false,
+          marker: false,
+          obj: account,
+        },
+      );
+    });
   }
 }
