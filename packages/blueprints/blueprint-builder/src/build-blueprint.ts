6f4d25b15225235f1902de8cd15dc6ad34d9cafe
Alex Forsyth <alex-git@amazon.com>
2022-04-14 11:56:31 -0400

    chore(packages): release components and utils

--- packages/blueprints/blueprint-builder/src/build-blueprint.ts
+++ packages/blueprints/blueprint-builder/src/build-blueprint.ts
@@ -35,29 +35,30 @@ import * as fs from 'fs';
  * 1. This MUST be the only 'class' exported, as 'Blueprint'
  * 2. This Blueprint should extend another ParentBlueprint
  */
- export class Blueprint extends ParentBlueprint {
-    constructor(options_: Options) {
-      super(options_);
-      console.log(defaults);
-      /**
-       * This is a typecheck to ensure that the defaults passed in are of the correct type.
-       * There are some cases where the typecheck will fail, but the defaults will still be valid, such when using enums.
-       * you can override this ex. myEnum: defaults.myEnum as Options['myEnum'],
-       */
-      const typeCheck: Options = {
-        outdir: this.outdir,
-        ...defaults,
-      };
-      const options = Object.assign(typeCheck, options_);
+export class Blueprint extends ParentBlueprint {
+  constructor(options_: Options) {
+    super(options_);
+    console.log(defaults);
+    /**
+     * This is a typecheck to ensure that the defaults passed in are of the correct type.
+     * There are some cases where the typecheck will fail, but the defaults will still be valid, such when using enums.
+     * you can override this ex. myEnum: defaults.myEnum as Options['myEnum'],
+     */
+    const typeCheck: Options = {
+      outdir: this.outdir,
+      ...defaults,
+    };
+    const options = Object.assign(typeCheck, options_);
+    console.log(options);
 
-      // add a repository
-      const repo = new SourceRepository(this, { title: 'MyRepo' });
+    // add a repository
+    const repo = new SourceRepository(this, { title: 'MyRepo' });
 
-      // example showing add files to the repository
-      // assets get synth'd from the 'assets' folder. At synth time, the asset folder is a sibling of the blueprint.ts.
-      const filecontent = fs.readFileSync('./assets/put-your-sample-assets-here.txt').toString();
-      new SourceFile(repo, 'path/to/copied-file.txt', filecontent);
-   }
+    // example showing add files to the repository
+    // assets get synth'd from the 'assets' folder. At synth time, the asset folder is a sibling of the blueprint.ts.
+    const filecontent = fs.readFileSync('./assets/put-your-sample-assets-here.txt').toString();
+    new SourceFile(repo, 'path/to/copied-file.txt', filecontent);
+  }
 }
 `;
 };
