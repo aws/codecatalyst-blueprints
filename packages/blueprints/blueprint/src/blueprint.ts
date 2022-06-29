78d43101b7425e921ce96dee1a1f87ca4be5f845
Tommy Ramos <tramos.usg@gmail.com>
2022-06-29 13:51:46 -0700

    feat: blueprintSynthesisError method (#138)
    * feat: blueprintSynthesisError method on base blueprint
    
    Co-authored-by: Tommy Ramos <ramosth@amazon.com>

--- packages/blueprints/blueprint/src/blueprint.ts
+++ packages/blueprints/blueprint/src/blueprint.ts
@@ -36,4 +36,35 @@ export class Blueprint extends Project {
       component.synthesize = () => {};
     }
   }
+
+  throwSynthesisError(error: BlueprintSynthesisError) {
+    throw error;
+  }
+}
+
+export enum BlueprintSynthesisErrorTypes {
+  ValidationError = 'ValidationError',
+  ConflictError = 'ConflictError',
+  NotFoundError = 'NotFoundError',
+  UndefinedSynthesisError = 'UndefinedSynthesisError',
+}
+
+export class BlueprintSynthesisError extends Error {
+  constructor(options: { message: string; type?: BlueprintSynthesisErrorTypes }) {
+    const { message, type } = options;
+    super(message);
+    switch (type) {
+      case BlueprintSynthesisErrorTypes.ValidationError:
+        this.name = type;
+        break;
+      case BlueprintSynthesisErrorTypes.ConflictError:
+        this.name = type;
+        break;
+      case BlueprintSynthesisErrorTypes.NotFoundError:
+        this.name = type;
+        break;
+      default:
+        this.name = BlueprintSynthesisErrorTypes.UndefinedSynthesisError;
+    }
+  }
 }
