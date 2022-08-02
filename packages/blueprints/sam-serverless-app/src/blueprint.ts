bb3cefa550c427fd9a997fce774afe9fafa7d167
Tommy Ramos <tramos.usg@gmail.com>
2022-08-02 14:12:28 -0700

    fix: sam requests module when running python tests (#148)
    Co-authored-by: Tommy Ramos <ramosth@amazon.com>

--- packages/blueprints/sam-serverless-app/src/blueprint.ts
+++ packages/blueprints/sam-serverless-app/src/blueprint.ts
@@ -310,7 +310,7 @@ VENV="venv"
 
 test -d $VENV || python3 -m venv $VENV || return
 $VENV/bin/pip install -r requirements-dev.txt
-
+$VENV/bin/pip install -r ${this.options.lambda.functionName}/hello_world/requirements.txt
 . $VENV/bin/activate`,
     });
   }
