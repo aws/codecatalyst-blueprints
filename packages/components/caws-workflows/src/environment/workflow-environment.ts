a5e4318ecb158ff1d30400811fa8ec2b47b1664b
Alex Forsyth <alex-git@amazon.com>
2022-09-01 00:38:34 -0400

    fix(component-workflow): update the workflows component interaction with environment

--- packages/components/caws-workflows/src/environment/workflow-environment.ts
+++ packages/components/caws-workflows/src/environment/workflow-environment.ts
@@ -1,4 +1,4 @@
-import { Environment } from '@caws-blueprint-component/caws-environments';
+import { AccountConnection, Environment } from '@caws-blueprint-component/caws-environments';
 
 export interface WorkflowEnvironment {
   Name: string;
@@ -10,12 +10,17 @@ export interface ConnectionDefinition {
   Role: string;
 }
 
-export function convertToWorkflowEnvironment(environment: Environment): WorkflowEnvironment {
+export function convertToWorkflowEnvironment(environment: Environment | undefined): WorkflowEnvironment | undefined {
+  if (!environment) {
+    return undefined;
+  }
+
   const connections: ConnectionDefinition[] = [];
-  environment.accountNames.forEach(accountName => {
-    environment.getRoles(accountName).forEach(role => {
+  (environment.accountKeys || []).forEach(key => {
+    const account = environment.definition[key] as AccountConnection<any>;
+    (environment.getRoles(key) || []).forEach(role => {
       connections.push({
-        Name: accountName || '',
+        Name: account.name || '',
         Role: role.name || '',
       });
     });
