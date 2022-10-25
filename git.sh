9295fd6098fad6f9b24510e02b781c02ec3b5ff4
alexforsyth <alexforsyth@no-reply.com>
2022-10-25 13:17:02 -0400

    chore(release): Tue Oct 25 17:17:02 UTC 2022

--- git.sh
+++ git.sh
@@ -1,10 +1,10 @@
 export username=`aws secretsmanager get-secret-value --secret-id blueprintsGithubUsername | jq -r .SecretString`
 export token=`aws secretsmanager get-secret-value --secret-id blueprintsGithubToken | jq -r .SecretString`
-git config --global user.email "${username}",
+git config --global user.email "${username}@no-reply.com",
 git config --global user.name "${username}",
 export repoName='aws/caws-blueprints'
 
-export triggeringHash='1250de3ff29b12ac1eb53fcf605bf3c0db9d9631'
+export triggeringHash='684675653bd0fec291c533b2b0402227e5e4c1ee'
 export repository="https://$username:$token@github.com/$repoName.git"
 export topRemoteHash=`git ls-remote ${repository} HEAD | awk '{ print $1}'`
 echo "triggeringHash [${triggeringHash}]"
@@ -21,6 +21,8 @@ git add -A
 CURRENT_DATE=$(date -u)
 git commit -m "chore(release): $CURRENT_DATE"
 git fetch origin main
-git pull --rebase origin main -s recursive -X ours --allow-unrelated-histories --no-edit
-exit 1
+git diff
+git pull --rebase origin main -s recursive -X theirs --allow-unrelated-histories --no-edit
+# git log
+# exit 1
 git push --set-upstream origin main --no-verify
