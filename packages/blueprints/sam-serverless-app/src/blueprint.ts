45183531fd8e04288f0ba43a4547ed02e18b4211
alexforsyth <alexforsyth@no-reply.com>
2022-11-11 11:26:15 -0500

    fix: fixes the build

--- packages/blueprints/sam-serverless-app/src/blueprint.ts
+++ packages/blueprints/sam-serverless-app/src/blueprint.ts
@@ -60,7 +60,7 @@ export interface Options extends ParentOptions {
        * This is the role that will be used to deploy the application. It should have access to deploy all of your resources. See the Readme for more information.
        * @displayName Deploy role
        * @inlinePolicy ./inline-policy-deploy.json
-       * @trustPolicy ./trust-policy-build.json
+       * @trustPolicy ./trust-policy.json
        */
       deployRole: Role<['SAM Deploy']>;
 
@@ -68,7 +68,7 @@ export interface Options extends ParentOptions {
        * This is the role that allows build actions to access and write to Amazon S3, where your serverless application package is stored.
        * @displayName Build role
        * @inlinePolicy ./inline-policy-build.json
-       * @trustPolicy ./trust-policy-build.json
+       * @trustPolicy ./trust-policy.json
        */
       buildRole: Role<['SAM Build']>;
     }>;
