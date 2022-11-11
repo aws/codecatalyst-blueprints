6c304aa80f8822933baccae5634438c35105a4e6
alexforsyth <alexforsyth@no-reply.com>
2022-11-11 13:12:15 -0500

    fix: update the PR to check caller identity and append accountid to bucket for global unique/stability

--- packages/utils/blueprint-cli/src/image-upload-tool/upload-image-to-aws.ts
+++ packages/utils/blueprint-cli/src/image-upload-tool/upload-image-to-aws.ts
@@ -1,5 +1,7 @@
 import fs from 'fs';
 import path from 'path';
+// ES6+ example
+import { STSClient, GetCallerIdentityCommand } from '@aws-sdk/client-sts';
 
 import * as pino from 'pino';
 import * as yargs from 'yargs';
@@ -21,6 +23,19 @@ export interface Image {
 }
 
 /**
+ * Checks that the caller is auth'd to a specific aws account.
+ * @returns aws account ID
+ */
+const verifyAWSAccount = async (log: pino.BaseLogger): Promise<string> => {
+  const callerIdentity = await new STSClient({}).send(new GetCallerIdentityCommand({}));
+  log.info(`Publishing under account [${callerIdentity.Account}]`);
+  if (!callerIdentity.Account) {
+    throw new Error(`You aren't logged into an aws account. Account information: ${JSON.stringify(callerIdentity, null, 2)}`);
+  }
+  return callerIdentity.Account;
+};
+
+/**
  * Uploads specified image to S3 bucket, and returns CloudFront distribution URL to the image.
  */
 export const uploadImagePublicly = async (
@@ -34,8 +49,11 @@ export const uploadImagePublicly = async (
   imageUrl: string;
   imageName: string;
 }> => {
+  const accountId = await verifyAWSAccount(log);
+
   const fullOptions = {
-    bucketName: options?.bucketName || 'blueprint-image', // the names 'blueprint-images' and 'blueprint-buckets' can not be used
+    // by default we make buckets globally unique by accountId
+    bucketName: options?.bucketName || `blueprint-image-${accountId}`,
     region: options?.region || 'us-west-2',
   };
 
