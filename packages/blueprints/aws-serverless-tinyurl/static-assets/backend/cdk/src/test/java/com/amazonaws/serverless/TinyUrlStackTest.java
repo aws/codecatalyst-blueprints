21f96ba286d1e4955d76cc06081802ed84b326f8
imajithkv <ajithkumar129@gmail.com>
2022-11-18 18:18:04 -0800

    Adding unit test for cdk stack

---
+++ packages/blueprints/aws-serverless-tinyurl/static-assets/backend/cdk/src/test/java/com/amazonaws/serverless/TinyUrlStackTest.java
@@ -0,0 +1,24 @@
+package com.amazonaws.serverless;
+
+
+import org.junit.jupiter.api.Test;
+import software.amazon.awscdk.App;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.assertions.Template;
+
+public class TinyUrlStackTest {
+
+    @Test
+    public void verify_app_synth() {
+        App app = new App();
+        TinyUrlAppStack stack = new TinyUrlAppStack(app, "TestStack", StackProps.builder().build());
+
+        Template template = Template.fromStack(stack);
+
+        template.resourceCountIs("AWS::Lambda::Function", 2);
+
+        template.hasResource("AWS::DynamoDB::Table", 1);
+
+        template.hasResource("AWS::ApiGateway::RestApi", 1);
+    }
+}
