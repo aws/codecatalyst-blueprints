6b1807f72a5ee15495cc8f0e07c3ffad67f53491
huzzyg <116315550+huzzyg@users.noreply.github.com>
2022-11-21 16:15:43 -0800

    Update action-cdk-deploy.ts

--- packages/components/caws-workflows/src/actions/action-cdk-deploy.ts
+++ packages/components/caws-workflows/src/actions/action-cdk-deploy.ts
@@ -1,28 +1,22 @@
 import { Blueprint } from '@caws-blueprint/blueprints.blueprint';
 import { WorkflowEnvironment } from '../environment/workflow-environment';
 import { WorkflowDefinition } from '../workflow/workflow';
-import { getDefaultActionIdentifier, ActionIdentifierAlias, ActionDefiniton } from './action';
-
-export interface CdkDeployInputConfiguration {
-  Artifacts?: string[];
-  Sources?: string[];
-  [key: string]: any;
-}
+import { getDefaultActionIdentifier, ActionIdentifierAlias, ActionDefiniton, ComputeConfiguration, OutputDefinition, InputsDefinition } from './action';
 
 export interface CdkDeployActionConfiguration {
-  [key: string]: string | undefined;
   StackName: string;
   Region?: string;
   Tags?: string;
   Context?: string;
   CfnOutputVariables?: string;
-  CdkRootLocation?: string;
+  CdkRootPath?: string;
 }
 
 export interface CdkDeployActionParameters {
-  inputs: CdkDeployInputConfiguration;
+  inputs: InputsDefinition;
+  outputs: OutputDefinition;
   environment: WorkflowEnvironment;
-  computeName?: 'Linux.x86-64.Large' | 'Linux.x86-64.XLarge' | 'Linux.x86-64.2XLarge' | string;
+  computeName?: ComputeConfiguration;
   configuration: CdkDeployActionConfiguration;
   actionName: string;
 }
@@ -33,12 +27,13 @@ export function addGenericCdkDeployAction(
     workflow: WorkflowDefinition;
   },
 ): string {
-  const { blueprint, workflow, inputs, environment, configuration, computeName } = params;
+  const { blueprint, workflow, inputs, outputs, environment, configuration, computeName } = params;
   const actionName = (params.actionName || 'DeployCdkStack').replace(new RegExp('-', 'g'), '_');
 
   const cdkDeployAction: ActionDefiniton = {
     Identifier: getDefaultActionIdentifier(ActionIdentifierAlias.cdkDeploy, blueprint.context.environmentId),
     Inputs: inputs,
+    Outputs: outputs,
     Environment: environment,
     Compute: computeName,
     Configuration: configuration,
