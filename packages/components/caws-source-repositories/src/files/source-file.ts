af55002ffafae05176acdaa92630839018de1218
alexforsyth <alex-git@amazon.com>
2023-07-27 14:15:15 -0400

    chore: top-level re-export of all blueprint packages

--- packages/components/caws-source-repositories/src/files/source-file.ts
+++ packages/components/caws-source-repositories/src/files/source-file.ts
@@ -1,7 +1,7 @@
 import { File } from './file';
 import { SourceRepository } from '../repository';
 
-export interface Options {
+export interface SourceFileOptions {
   /**
    * Mark this file as readonly.
    * @deprecated - declare readonly status through the ownership file construct
@@ -14,7 +14,7 @@ export class SourceFile extends File {
   sourceRepository: SourceRepository;
   filepath: string;
 
-  constructor(sourceRepository: SourceRepository, filePath: string, content: string, _options?: Options) {
+  constructor(sourceRepository: SourceRepository, filePath: string, content: string, _options?: SourceFileOptions) {
     sourceRepository._trackFile(filePath, Buffer.from(content, 'utf8'));
     super(sourceRepository, filePath, Buffer.from(content));
     this.sourceRepository = sourceRepository;
---
+++ packages/top-level/codecatalyst-blueprints/src/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Re-export the base blueprint
+ */
+export * from '@caws-blueprint/blueprints.blueprint';
+
+/**
+ * re-export the blueprint components namespaced under each component
+ */
+export * from '@caws-blueprint-component/caws-workflows';
+export * from '@caws-blueprint-component/caws-source-repositories';
+export * from '@caws-blueprint-component/caws-workspaces';
+export * from '@caws-blueprint-component/caws-environments';
+export * from '@caws-blueprint-component/caws-issues';
