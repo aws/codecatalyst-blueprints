e902d866878e36ce4972f532637f461ffe9461c3
Xiao Chen <xxchen@amazon.com>
2023-09-11 15:12:01 -0400

    feat(cli: upload-blueprint): replace createBlueprintUploadUrl

--- packages/utils/blueprint-cli/src/publish/upload-blueprint.ts
+++ packages/utils/blueprint-cli/src/publish/upload-blueprint.ts
@@ -7,22 +7,19 @@ import * as pino from 'pino';
 import { CodeCatalystAuthentication, generateHeaders } from './codecatalyst-authentication';
 import { IdentityResponse } from './verify-identity';
 
-const mb = 1_024_000;
-
-interface PublishingJob {
-  publishingJobId: string;
-  uploadUrl: string;
-}
-
-export async function uploadBlueprint(log: pino.BaseLogger, packagePath: string, endpoint: string, blueprint: {
-  publishingSpace: string;
-  targetSpace: string;
-  packageName: string;
-  version: string;
-  authentication: CodeCatalystAuthentication;
-  identity: IdentityResponse;
-}) {
-
+export async function uploadBlueprint(
+  log: pino.BaseLogger,
+  packagePath: string,
+  endpoint: string,
+  blueprint: {
+    publishingSpace: string;
+    targetSpace: string;
+    packageName: string;
+    version: string;
+    authentication: CodeCatalystAuthentication;
+    identity: IdentityResponse;
+  },
+) {
   // get the signature of the package
   const pipeline = util.promisify(stream.pipeline);
   const hash = crypto.createHash('sha384').setEncoding('hex');
@@ -35,16 +32,16 @@ export async function uploadBlueprint(log: pino.BaseLogger, packagePath: string,
     process.exit(254);
   }
 
-  const gqlResponse = await axios.default.post(
+  log.info(`Start publishing blueprint package to ${blueprint.targetSpace}.`);
+
+  const gqlPublishBlueprintPackageResponse = await axios.default.post(
     `https://${endpoint}/graphql?`,
     {
       query: `mutation {
-        createBlueprintUploadUrl(input: {
+        publishBlueprintPackage(input: {
           spaceName: "${blueprint.targetSpace}",
-          publisher: "${blueprint.publishingSpace}",
-          packageSignature: "${packageSignature}",
-          name: "${blueprint.packageName.split('.').pop()}",
-          versionId: "${blueprint.version}" }) {
+          blueprintName: "${blueprint.packageName.split('.').pop()}",
+          version: "${blueprint.version}" }) {
             uploadUrl, publishingJobId }
           }`,
     },
@@ -58,25 +55,59 @@ export async function uploadBlueprint(log: pino.BaseLogger, packagePath: string,
       },
     },
   );
-  if (gqlResponse.status != 200) {
-    log.error('unable to retrive upload url: %s', gqlResponse.data);
-    process.exit(254);
-  } else if (gqlResponse.data.errors?.[0]) {
-    log.error(gqlResponse.data.errors[0]);
+  if (gqlPublishBlueprintPackageResponse.status != 200) {
+    log.error('Failed to publish blueprint package: %s', gqlPublishBlueprintPackageResponse.status);
     process.exit(254);
   }
 
-  const publishingJob = gqlResponse.data.data.createBlueprintUploadUrl as PublishingJob;
-  log.info('Starting publishing job id: %s', publishingJob.publishingJobId);
+  const publishingStatusId = gqlPublishBlueprintPackageResponse.data.data.publishingStatusId;
+  log.info('Published blueprint package with status Id: %s, checking publishing status.', publishingStatusId);
 
-  const uploadResponse = await axios.default.put(publishingJob.uploadUrl, fs.readFileSync(packagePath), {
-    // allow publishing of packages up to 5 gb
-    maxBodyLength: 5 * 1000 * mb,
-  });
-  if (uploadResponse.status != 200) {
-    log.error('failed to upload blueprint package: %s', uploadResponse.status);
-    process.exit(254);
+  let retries = 0;
+  let gqlGetPublishBlueprintStatusResponse;
+  let publishingBlueprintStatus;
+  while (retries < 5) {
+    gqlGetPublishBlueprintStatusResponse = await axios.default.post(
+      `https://${endpoint}/graphql?`,
+      {
+        query: `mutation {
+        getPublishBlueprintStatus(input: {
+          spaceName: "${blueprint.targetSpace}",
+          blueprintName: "${blueprint.packageName.split('.').pop()}",
+          id: "${publishingStatusId}",
+          version: "${blueprint.version}" }) {
+            uploadUrl, publishingJobId }
+          }`,
+      },
+      {
+        headers: {
+          'authority': endpoint,
+          'origin': `https://${endpoint}`,
+          'accept': 'application/json',
+          'content-type': 'application/json',
+          ...generateHeaders(blueprint.authentication, blueprint.identity),
+        },
+      },
+    );
+
+    publishingBlueprintStatus = gqlGetPublishBlueprintStatusResponse.data.data.status;
+
+    if (publishingBlueprintStatus == 'SUCCEEDED') {
+      log.info('Successfully published blueprint package');
+      return;
+    } else if (publishingBlueprintStatus == 'FAILED') {
+      log.error(
+        'Failed to publish blueprint package: %s with reason: %s',
+        gqlGetPublishBlueprintStatusResponse.status,
+        gqlGetPublishBlueprintStatusResponse.data.data.reason,
+      );
+      process.exit(254);
+    } else if (publishingBlueprintStatus == 'CANCELED') {
+      log.info('Blueprint package publishing process is canceled.');
+    } else {
+      // wait for 2 seconds if the returned status is in progress
+      await new Promise(completed => setTimeout(completed, 2000));
+      retries++;
+    }
   }
-
-  log.info('Uploaded package for processing!');
 }
