ac69c1c08ef27e6f39670f8769cbcf493965d1d7
Tommy Ramos <tramos.usg@gmail.com>
2023-10-04 16:25:52 -0400

    Feat(workspaces): Set default to latest mde image (#394)
    * feat(workspaces): change default to latest mde image
    ---------
    
    Co-authored-by: Thomas Ramos <ramosth@amazon.com>

--- packages/components/caws-workspaces/src/samples/index.ts
+++ packages/components/caws-workspaces/src/samples/index.ts
@@ -15,7 +15,39 @@ export class SampleWorkspaces {
       {
         name: 'aws-runtime',
         container: {
-          image: 'public.ecr.aws/aws-mde/universal-image:1.0',
+          image: 'public.ecr.aws/aws-mde/universal-image:3.0',
+          mountSources: true,
+          volumeMounts: [
+            {
+              name: 'docker-store',
+              path: '/var/lib/docker',
+            },
+          ],
+        },
+      },
+      {
+        name: 'docker-store',
+        volume: {
+          size: '16Gi',
+        },
+      },
+    ],
+  };
+  static readonly latest: WorkspaceDefinition = {
+    schemaVersion: '2.0.0',
+    metadata: {
+      name: 'aws-universal',
+      version: '1.0.1',
+      displayName: 'AWS Universal',
+      description: 'Stack with AWS Universal Tooling',
+      tags: ['aws', 'a12'],
+      projectType: 'aws',
+    },
+    components: [
+      {
+        name: 'aws-runtime',
+        container: {
+          image: 'public.ecr.aws/aws-mde/universal-image:latest',
           mountSources: true,
           volumeMounts: [
             {
