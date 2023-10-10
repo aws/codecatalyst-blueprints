6b9a07aa5dce9549ad0f82ae02a48186762769f0
Alexander Forsyth <alex@alexforsyth.io>
2023-10-10 14:15:35 -0400

    fix: remove unused serverless-tinyurl blueprint (#404)

--- packages/blueprints/serverless-tinyurl/static-assets/backend/jacocoConsoleReporter.sh
+++
@@ -1,8 +0,0 @@
-for file in $(find . -type f -name "jacoco.csv"); do
-    awk -F, \
-    '{ instructions += $4 + $5; covered += $5 } END \
-     { print "jacoco coverage:  " FILENAME "\n" covered, "/", instructions, " instructions covered"; \
-     print 100*covered/instructions, "% covered" }' \
-    $file
-done
-
