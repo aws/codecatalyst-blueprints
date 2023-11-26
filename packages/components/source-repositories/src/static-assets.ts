8b06933299184875bdf5069ae50633f7f02b88f7
alexforsyth <alex-git@amazon.com>
2023-11-26 09:00:19 -0500

    feat: support copy-from static assets

--- packages/components/source-repositories/src/static-assets.ts
+++ packages/components/source-repositories/src/static-assets.ts
@@ -2,6 +2,8 @@ import * as fs from 'fs';
 import * as pathing from 'path';
 import * as glob from 'glob';
 import * as Mustache from 'mustache';
+import { File } from './files/file';
+import { SourceRepository } from './repository';
 
 const STATIC_ASSET_DIRECTORY = 'static-assets';
 
@@ -18,6 +20,37 @@ export class StaticAsset {
         ...globOptions,
       })
       .map(path => new this(path));
+  }
+
+  /**
+   * Copies all files from a subdirectory. This should be a path to a directory inside the static assets folder. This creates a new file in the repository for each file under the directory.
+   * @example frontend
+   * @param directoryPath - directory under static-assets
+   * @param to - repository into which we're putting the files
+   * @param options
+   */
+  static copyFrom(
+    directoryPath: string,
+    to: SourceRepository,
+    options?: {
+      globOptions?: any;
+      /**
+       * apply a transformation across each file;
+       */
+      transformation?: (file: StaticAsset) => { location?: string; content?: Buffer };
+    },
+  ) {
+    const directory = pathing.join(directoryPath);
+    StaticAsset.findAll(pathing.join(directory, '**/*'), options?.globOptions).forEach(file => {
+      let location = file.path().replace(directory, '');
+      let content = file.content();
+      if (options?.transformation) {
+        const result = options.transformation(file);
+        location = result.location || location;
+        content = result.content || content;
+      }
+      new File(to, location, content);
+    });
   }
 
   private content_: Buffer;
@@ -47,6 +80,33 @@ export class StaticAsset {
  * Asset that can perform a mustache subsitution on the content.
  */
 export class SubstitionAsset extends StaticAsset {
+  /**
+   * Copies all files from a subdirectory. This should be a path to a directory inside the static assets folder. This creates a new file in the repository for each file under the directory.
+   * @example findAll(frontend, myrepo)
+   * @param directoryPath - directory under static-assets
+   * @param to - repository into which we're putting the files
+   * @param options
+   */
+  static copyFrom(
+    directoryPath: string,
+    to: SourceRepository,
+    options?: {
+      globOptions?: any;
+      /**
+       * Allows for Mustache subsitution across assets.
+       */
+      subsitution?: any;
+    },
+  ) {
+    StaticAsset.copyFrom(directoryPath, to, {
+      transformation: file => {
+        return {
+          content: Buffer.from(Mustache.render(file.content().toString(), options?.subsitution)),
+        };
+      },
+    });
+  }
+
   constructor(path: string) {
     super(path);
   }
