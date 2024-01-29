c2bc73d32fa5b702a3df7920fdf4904e36fe8796
Alex Alpert <aalpert@amazon.com>
2024-01-29 14:43:32 -0500

    fix: dependencies

--- packages/components/secrets/src/secret-definition.ts
+++ packages/components/secrets/src/secret-definition.ts
@@ -1,3 +1,6 @@
+/**
+ * Generates a Secret input in the blueprint wizard. The UI prompts the user for a name, value and optional description.
+ */
 export type SecretDefinition = {
   name: string;
   description?: string;
