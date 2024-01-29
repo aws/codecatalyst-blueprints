868267c5c9e64a5c71ed0b04d432f72c34629ded
Alexander Forsyth <alex@alexforsyth.io>
2024-01-29 14:54:36 -0500

    feat(blueprint-subscriber): adds a new blueprint, allows subscribing to packages on NPM as private blueprints (#468)
    * started
    
    * fix: blueprints
    
    * fix
    
    * fixes
    
    * feat(blueprint-importer): add a blueprint that allows users to import more blueprints
    
    * buiding
    
    * feat(subscribe-blueprint): subscribe to blueprint packages on NPM
    
    * fixes
    
    * readme

---
+++ packages/blueprints/subscribe-blueprint/.projenrc.ts
@@ -0,0 +1,37 @@
+import { ProjenBlueprint } from '@amazon-codecatalyst/blueprint-util.projen-blueprint';
+
+const project = new ProjenBlueprint({
+  authorName: 'blueprints',
+  publishingOrganization: 'blueprints',
+  packageName: '@amazon-codecatalyst/blueprints.subscribe-blueprint',
+  displayName: 'Subscribe to external blueprint',
+  description: 'Import external NPM packages as custom blueprints',
+  name: 'subscribe-blueprint',
+  defaultReleaseBranch: 'main',
+  projenrcTs: true,
+  sampleCode: false,
+  github: false,
+  eslint: true,
+  jest: false,
+  npmignoreEnabled: true,
+  tsconfig: {
+    compilerOptions: {
+      esModuleInterop: true,
+      noImplicitAny: false,
+    },
+  },
+  copyrightOwner: 'blueprints',
+  deps: [
+    '@amazon-codecatalyst/blueprints.blueprint',
+    'projen',
+    '@amazon-codecatalyst/blueprint-component.workflows',
+    '@amazon-codecatalyst/blueprint-component.source-repositories',
+    '@amazon-codecatalyst/blueprint-component.environments',
+  ],
+
+  devDeps: ['ts-node', 'typescript', '@amazon-codecatalyst/blueprint-util.projen-blueprint', '@amazon-codecatalyst/blueprint-util.cli'],
+  keywords: ['blueprint-subscriber', 'blueprint-publisher', 'external-blueprint', 'blueprint'],
+  homepage: '',
+});
+
+project.synth();
