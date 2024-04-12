89522a5c3a9ccc71e26fab7368984deeed28b857
Alex Alpert <aalpert@amazon.com>
2024-04-12 07:38:13 -0400

    fix: dynamic kv input

--- packages/blueprints/blueprint/src/ui-selectors/dynamic-kv-input.ts
+++ packages/blueprints/blueprint/src/ui-selectors/dynamic-kv-input.ts
@@ -1,27 +1,26 @@
-type DISPLAY_COMPONENT_TYPE =
+export type DISPLAY_COMPONENT_TYPE =
     'string' | 'number' | 'checkbox' | 'dropdown' | 'textarea' | 'label'
 
 
 export type DynamicKVInput = {
   key: string;
-  value: string | number;
-  label: string;
+  value: string | number | boolean | null;
   /** The type of component to display in the wizard.
     * default: 'string'
     */
-  displayComponent?: DISPLAY_COMPONENT_TYPE;
+  displayComponent?: DISPLAY_COMPONENT_TYPE | string;
 
   /**
    * Possible accepted values for dropdown type.
    */
   possibleValues?: string[];
   /**
-    * Whether the field is optional.
+     * If true, field is optional.
      * default: false
      */
   optional?: boolean;
   /**
-    * Text description to display beneath input.
+    * Text description displayed in wizard.
     */
   description?: string;
   /**
@@ -29,21 +28,21 @@ export type DynamicKVInput = {
    */
   validationRegex?: string;
   /**
-   * Constraint text to display below string input.
+   * If input in wizard does not satisfy validationRegex, this message is displayed under the field.
    */
   validationMessage?: string;
   /**
-   * Placeholder text to display in empty string or number.
+   * Placeholder text to display in empty input.
    */
   placeholderText?: string;
 
   /**
-   * Will generate n numbers and append to the value in the wizard.
+   * If specified, wizard will append n numbers to the value in the wizard.
    */
   defaultEntropy?: number;
 
   /**
-   * If true, will hide field from wizard.
+   * If true, field is hidden in the wizard.
    */
   hidden?: boolean;
 
