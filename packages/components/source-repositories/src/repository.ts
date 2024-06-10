37430447991f9ece318c919a4c6aacda7762258b
Mike Landi <miclanm@amazon.com>
2024-06-10 13:23:41 -0400

    feat(repository): support object based copy substitutions

--- packages/components/source-repositories/src/repository.ts
+++ packages/components/source-repositories/src/repository.ts
@@ -64,7 +64,7 @@ export class SourceRepository extends Component {
      * @param subsitution
      * @returns
      */
-    substitute?: { [key: string]: string };
+    substitute?: { [key: string]: string | object };
 
     /**
      * Allows iteration across all copied files. This is run after substitutions and final path resolution and prior to initializing the file object.
