2045692466e7142a9a0033abd1c7ca850176d111
Xiao Chen <xxchen@amazon.com>
2025-01-07 10:53:24 -0500

    fix: update builder blueprint workflow build aciton image

--- packages/blueprints/blueprint-builder/src/build-release-workflow.ts
+++ packages/blueprints/blueprint-builder/src/build-release-workflow.ts
@@ -47,7 +47,7 @@ export function buildReleaseWorkflow(workflow: WorkflowBuilder, options?: { incl
     steps: ["if $IS_RELEASE_COMMIT; then  echo 'This is a release commit, skipping'; else chmod +x release.sh && ./release.sh; fi"],
     container: {
       Registry: 'CODECATALYST',
-      Image: 'CodeCatalystLinux_x86_64:2022_11',
+      Image: 'CodeCatalystLinux_x86_64:2024_03',
     },
   });
 
